502f7e74c6152a742b448048d105d67d12bc1467
Tomás Andrade <tomas.andrade2043@gmail.com>
2024-04-09 01:22:29 +0100

    +Tooltips

--- components/about-me-section.tsx
+++ components/about-me-section.tsx
@@ -3,6 +3,12 @@ import { Button } from "@/components/ui/button";
 import { Github, Linkedin, Mail, Mails, Phone, Twitter } from "lucide-react";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export const AboutMeSection = () => {
   return (
@@ -32,7 +38,7 @@ export const AboutMeSection = () => {
             <li className=" ms-4">
               <div className="absolute w-3 h-3 bg-primary rounded-full mt-1.5 -start-1.5 border border-primary"></div>
               <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                Abril 2024 - 2024 
+                Abril 2024 - 2024
               </time>
               <h3 className="text-lg font-semibold text-white">
                 Estágio na Zarph
@@ -51,38 +57,72 @@ export const AboutMeSection = () => {
         >
           <h1 className="font-bold text-white text-3xl pl-1">Contatos</h1>
           <div className="grid grid-cols-3 gap-2">
-            <Link href={"https://github.com/Tomas2043"}>
-              <Button className="w-full text-white">
-                <Github className="size-5" />
-              </Button>
-            </Link>
-            <Link
-              href={"https://www.linkedin.com/in/tom%C3%A1s-andrade-6a449a241/"}
-            >
-              <Button className="w-full text-white">
-                <Linkedin className="size-5" />
-              </Button>
-            </Link>
-            <Link href={"https://twitter.com/Toms2043"}>
-              <Button className="w-full text-white">
-                <Twitter className="size-5" />
-              </Button>
-            </Link>
-            <Link href={"mailto:tomas.andrade2043@gmail.com"}>
-              <Button className="w-full text-white">
-                <Mail className="size-5" />
-              </Button>
-            </Link>
-            <Link href={"mailto:aluno221418@epad.edu.pt"}>
-              <Button className="w-full text-white">
-                <Mails className="size-5" />
-              </Button>
-            </Link>
-            <Link href={"tel:936314098"}>
-              <Button className="w-full text-white">
-                <Phone className="size-5" />
-              </Button>
-            </Link>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Link href={"https://github.com/Tomas2043"}>
+                    <Button className="w-full text-white">
+                      <Github className="size-5" />
+                    </Button>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent>Github</TooltipContent>
+              </Tooltip>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Link
+                    href={
+                      "https://www.linkedin.com/in/tom%C3%A1s-andrade-6a449a241/"
+                    }
+                  >
+                    <Button className="w-full text-white">
+                      <Linkedin className="size-5" />
+                    </Button>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent>LinkedIn</TooltipContent>
+              </Tooltip>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Link href={"https://twitter.com/Toms2043"}>
+                    <Button className="w-full text-white">
+                      <Twitter className="size-5" />
+                    </Button>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent>Twitter</TooltipContent>
+              </Tooltip>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Link href={"mailto:tomas.andrade2043@gmail.com"}>
+                    <Button className="w-full text-white">
+                      <Mail className="size-5" />
+                    </Button>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent>Email Pessoal</TooltipContent>
+              </Tooltip>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Link href={"mailto:aluno221418@epad.edu.pt"}>
+                    <Button className="w-full text-white">
+                      <Mails className="size-5" />
+                    </Button>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent>Email Escolar</TooltipContent>
+              </Tooltip>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Link href={"tel:936314098"}>
+                    <Button className="w-full text-white">
+                      <Phone className="size-5" />
+                    </Button>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent>Telefone</TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </div>
         </motion.div>
       </section>
